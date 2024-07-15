b5a8207ae56065ba441231b3cacf5f93c484867d
Gabriel-Fiuza <gpaulinellifiuza@gmail.com>
2024-07-15 17:05:30 -0300

    styles added but not finished

--- src/index.js
+++ src/index.js
@@ -12,21 +12,26 @@ app.set('views', path.join(__dirname, 'views'))
 const children = database
 
 app.get('/', (req, res) => {
-    res.render('home', )
+    res.render('home')
 })
 
 app.get('/register', (req, res) => {
     res.render('form')
 })
 
+app.get('/overview', (req, res) => {
+    res.render('overview', { database: database })
+})
+
 app.post('/sucess', (req, res) => {
     const childName = req.body.name
+    const shoeNumber = req.body.number
     const childAge = +req.body.age
     const gender = req.body.gender
     const clothing = +req.body.clothing
     const motherName = req.body.motherName
 
-    children.push({ childName, childAge, gender, clothing, motherName })
+    children.push({ childName, shoeNumber, childAge, gender, clothing, motherName })
     fs.writeFile(path.join(__dirname, 'database.json'), JSON.stringify(children), (err) => {
         if(err){
             console.log(err.message)
