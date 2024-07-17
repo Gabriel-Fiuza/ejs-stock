49d4823fdc8bd2f2782ace1cc9b1a617bae4557a
Gabriel Fiuza <gpaulinellifiuza@gmail.com>
2024-07-16 23:29:33 -0300

    'delete' and 'search' method added, automatic age calculation, some styles added but not concluded and database sorted by the shoe number

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 const express = require('express')
 const path = require('node:path')
 const fs = require('node:fs')
-const database = require('./database.json')
+let database = require('./database.json')
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-let numToFilter = []
-
-const children = database
+let numToFilter
 let filteredArray
+let date = new Date().getFullYear()
+database.sort((child1, child2) => child1.shoeNumber - child2.shoeNumber)
+
 
 app.get('/', (req, res) => {
     res.render('home')
 })
+
 
 app.get('/register', (req, res) => {
     res.render('form')
@@ -30,31 +33,41 @@ app.get('/filtragem', (req,res) => {
     res.render('filter', { filteredArray: filteredArray })
 })
 
+app.post('/delete', (req, res) => {
+    const idToDelete = +req.body.delete
+    const deletedChildArray = database.filter((child) => {
+        return child.childID !== idToDelete
+    })
+    database = deletedChildArray
+    res.redirect('/overview')
+})
+
 app.post('/toFilter', (req, res) => {
-    numToFilter.push(+req.body.numberToFilter)
+    numToFilter = +req.body.numberToFilter
     const filtering = database.filter(child => {
-        return child.shoeNumber === numToFilter[0]
+        return child.shoeNumber === numToFilter
     })
     filteredArray = filtering
-    console.log(filteredArray)
     res.redirect('/filtragem')
-    numToFilter = []
+    numToFilter = null
 })
 
 app.post('/sucess', (req, res) => {
-    const childName = req.body.name
+    const childID = Math.round(Math.random() * 99999)
+    const childName = (req.body.name).toUpperCase()
     const shoeNumber = +req.body.number
-    const childAge = +req.body.age
+    const childAge =  date - new Date(req.body.birthdate).getFullYear()
     const gender = req.body.gender
     const clothing = +req.body.clothing
-    const motherName = req.body.motherName
+    const motherName = (req.body.motherName).toUpperCase()
 
-    children.push({ childName, shoeNumber, childAge, gender, clothing, motherName })
-    fs.writeFile(path.join(__dirname, 'database.json'), JSON.stringify(children), (err) => {
+    database.push({ childID, childName, shoeNumber, childAge, gender, clothing, motherName })
+    fs.writeFile(path.join(__dirname, 'database.json'), JSON.stringify(database), (err) => {
         if(err){
             console.log(err.message)
         }
     } )
+    database.sort((child1, child2) => child1.shoeNumber - child2.shoeNumber)
 
     res.redirect('/register')
 })
